016d82c430dfb18a1deff5c26079603c12f0ff5b
Martin1248 <martinonlinedrive@gmail.com>
2019-01-22 14:56:06 +0100

    Create a unit test for database logic

---
+++ app/src/androidTest/java/io/github/martin1248/gtdlight2/DatabaseTest.java
@@ -0,0 +1,57 @@
+package io.github.martin1248.gtdlight2;
+
+import android.content.Context;
+import android.util.Log;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.room.Room;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
+import io.github.martin1248.gtdlight2.database.AppDatabase;
+import io.github.martin1248.gtdlight2.database.NoteDao;
+import io.github.martin1248.gtdlight2.database.NoteEntity;
+import io.github.martin1248.gtdlight2.utilities.SampleData;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class DatabaseTest {
+    public static final String TAG = "Junit";
+    private AppDatabase mDb;
+    private NoteDao mDao;
+
+    @Before
+    public void createDb() {
+        Context context = InstrumentationRegistry.getTargetContext();
+        mDb = Room.inMemoryDatabaseBuilder(context, AppDatabase.class).build();
+        mDao = mDb.noteDao();
+        Log.i(TAG, "createDb");
+    }
+
+    @After
+    public void closeDb() {
+        mDb.close();
+        Log.i(TAG, "closeDb");
+    }
+
+    @Test
+    public void createAndRetrieveNotes() {
+        mDao.insertAll(SampleData.getNotes());
+        int count = mDao.getCount();
+        Log.i(TAG, "createAndRetrieveNotes: count = " + count);
+        assertEquals(SampleData.getNotes().size(), count);
+    }
+
+    @Test
+    public void compareStrings() {
+        mDao.insertAll(SampleData.getNotes());
+        NoteEntity original = SampleData.getNotes().get(0);
+        NoteEntity fromDb = mDao.getNoteById(1);
+        assertEquals(original.getText(), fromDb.getText());
+        assertEquals(1,fromDb.getId());
+    }
+}
